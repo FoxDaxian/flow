40d92850dc4c999b5daf2a08080fce612215a687
a13821190779 <945039036@qq.com>
2018-02-16 19:16:29 +0800

    Update: 函数参数类型情况

--- src/index.js
+++ src/index.js
@@ -1,7 +1,58 @@
 // @flow
 
-function foo(person: { name: string, age?: number }) {
-    const { age } = person;
-    console.log(age);
+// 返回值和传入的值类型一致，固定写法
+// function identity<T>(value: T): T {
+//     return value;
+// }
+
+// let foo = 42;
+
+// function mutate() {
+//     foo = true;
+//     foo = 'hello';
+// }
+
+// mutate();
+// let name: string | number | boolean = foo;
+
+// function foo(name: string, age?: number, ...other: Array<number>) {
+//     console.log(name);
+//     console.log(age);
+//     console.log(other);
+// }
+// foo('fox', null, 1, 2, 3);
+
+// function foo(a, b): boolean {
+//     return !!a && !!b; 必须转为布尔
+// }
+// foo('1', '2');
+
+// function isString(y): %checks {
+//     return typeof y === 'string';
+// }
+
+// function isNumber(y): %checks {
+//     return typeof y === 'number';
+// }
+
+// function isNumberOrString(y): %checks {
+//     return isString(y) || isNumber(y);
+// }
+
+// function foo(x): string | number {
+//     if (isNumberOrString(x)) {
+//         return x + x;
+//     } else {
+//         return x.length;
+//     }
+// }
+
+// foo('a');
+// foo(5);
+// console.log(foo({}), 1);
+
+// 函数参数为函数时，可以用() => void | number | ...(类型)表示
+function method(func: () => Array<number>) {
+  
 }
-foo({ name: 'fox', age: 23 })
+method(() => [1])
